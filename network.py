7a558cb3be91f431629f6012919481dfe97b0b92
mechuser <mechuser@IMI-T400s.(none)>
2015-01-08 14:45:55 +0900

    calculateDelta

--- network.py
+++ network.py
@@ -18,14 +18,14 @@ for j in range(m_num):
     nm[j] = neuron.Neuron()
     for k in range(100):
         nm[j].connectPrev(ni[k],w[k])
-        ni[k].connectNext(nm[j])
+        ni[k].connectNext(nm[j],w[k])
 
 for j in range(out_num):
     no[j] = neuron.Neuron()
     no[j].generateDelta
     for k in range(m_num):
         no[j].connectPrev(nm[k],wo[k])
-        nm[k].connectNext(no[j])
+        nm[k].connectNext(no[j],wo[k])
 
 for j in range(m_num):
     nm[j].calculateDelta
